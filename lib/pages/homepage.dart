cf8e6f92fd6ae88b1bbe8e1e4e2a0fb0756bed9d
Harshit Rao <harshitdroid@gmail.com>
2023-05-02 15:47:35 -0700

    Added notification capability. Sends notification every 7 days to re do survey.(can be changed later)

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -6,6 +6,7 @@ import 'package:circle_app/components/linechart.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import '../classes/contact.dart';
 
@@ -31,7 +32,7 @@ class _HomePageState extends State<HomePage> {
     MyData(4, 3),
     MyData(11, 9),
   ];
-
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   // sign user out function
   void _handleSignOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
@@ -58,6 +59,38 @@ class _HomePageState extends State<HomePage> {
     throw Exception('Failed to update contacts');
   }
 }
+
+
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+
+    Future<void> _scheduleWeeklyNotification() async {
+      var scheduledNotificationDateTime = DateTime.now().add(Duration(days: 7));
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'weeklySurveyReminderChannel', 'Weekly Survey Reminder',
+          importance: Importance.high, priority: Priority.high);
+      var platformChannelSpecifics = NotificationDetails(
+          android: androidPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.schedule(
+          0,
+          'Weekly Survey Reminder',
+          'Don\'t forget to complete your weekly survey!',
+          scheduledNotificationDateTime,
+          platformChannelSpecifics);
+      await Future.delayed(Duration(days: 7)); // wait for 7 seconds
+      _scheduleWeeklyNotification(); // schedule the next notification
+    }
+
+    var initializationSettings =
+    InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    _scheduleWeeklyNotification();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
